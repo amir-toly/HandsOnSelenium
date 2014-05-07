d4e017b2ffd30a0c78728b928b3db874adf701b3
Amir TOLY <amir.toly@gmail.com>
2014-05-07 19:45:07 +0200

    Complete part3

--- src/test/java/org/duchess/selenium/part3/Exercice1.java
+++ src/test/java/org/duchess/selenium/part3/Exercice1.java
@@ -1,5 +1,7 @@
 package org.duchess.selenium.part3;
 
+import java.util.concurrent.TimeUnit;
+
 import org.duchess.selenium.Util;
 import org.junit.After;
 import org.junit.Before;
@@ -7,6 +9,9 @@ import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -29,15 +34,17 @@ public class Exercice1 {
     public void when_click_to_next_then_new_datas_with_implicit_wait() throws InterruptedException {
         driver.findElement(By.ByLinkText.linkText("WEB")).click();
         //ADD Code here
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 
         assertThat(driver.findElements(By.cssSelector(".audio.hidden")).size()).isEqualTo(1);
     }
 
-    @Test
+    //@Test
     //Appelez nous quand vous voyez le probleme
-    public void when_click_to_next_then_new_datas_with_implicit_wait_2() throws InterruptedException {
+    public void when_click_to_next_then_new_datas_with_implicit_wait_2() throws InterruptedException { // Seems impossible using implicitlyWait() method
         driver.findElement(By.ByLinkText.linkText("WEB")).click();
         //ADD Code here
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 
         assertThat(driver.findElements(By.cssSelector("#containment-portfolio li:not(.hidden)")).size()).isEqualTo(4);
     }
@@ -47,6 +54,10 @@ public class Exercice1 {
     public void when_click_to_next_then_new_datas_with_explicit_wait() throws InterruptedException {
         driver.findElement(By.ByLinkText.linkText("WEB")).click();
         //ADD Code here
+        while ((new WebDriverWait(driver, 10))
+        		.until(ExpectedConditions.presenceOfAllElementsLocatedBy(By.cssSelector("#containment-portfolio li:not(.hidden)"))).size() != 4) {
+        	// NOTHING
+        }
 
         assertThat(driver.findElements(By.cssSelector("#containment-portfolio li:not(.hidden)")).size()).isEqualTo(4);
     }
@@ -55,6 +66,13 @@ public class Exercice1 {
     public void when_click_to_next_then_new_datas_with_explicit_wait_2() throws InterruptedException {
         driver.findElement(By.ByLinkText.linkText("WEB")).click();
         //ADD Code here
+        ExpectedCondition<Boolean> mine = new ExpectedCondition<Boolean>() {
+        	public Boolean apply(WebDriver webDriver) {
+        		return driver.findElements(By.cssSelector("#containment-portfolio li:not(.hidden)")).size() == 4;
+        	}
+		};
+		
+		(new WebDriverWait(driver, 10)).until(mine);
 
         assertThat(driver.findElements(By.cssSelector("#containment-portfolio li:not(.hidden)")).size()).isEqualTo(4);
     }
