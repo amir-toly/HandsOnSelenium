33e2274443768624e02c46bb39026d27cdc9d8a9
Amir TOLY <amir.toly@gmail.com>
2014-05-12 19:23:53 +0200

    Complete (sort of, poor usage of WebElement class) part4 - exercise 2

--- src/test/java/org/duchess/selenium/part4/ex2/Exercice2.java
+++ src/test/java/org/duchess/selenium/part4/ex2/Exercice2.java
@@ -1,14 +1,10 @@
 package org.duchess.selenium.part4.ex2;
 
-import org.duchess.selenium.Util;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.support.ui.ExpectedCondition;
-import org.openqa.selenium.support.ui.WebDriverWait;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -26,20 +22,14 @@ public class Exercice2 {
     @Before
     public void beforeTest() {
         driver = new FirefoxDriver();
-        driver.get(Util.getPortfolio2());
+        portofolioPage = new PortofolioPage(driver);
     }
 
     @Test
     public void when_click_to_next_then_new_datas_with_implicit_wait() {
-        driver.findElement(By.ByLinkText.linkText("WEB")).click();
-        (new WebDriverWait(driver, 10))
-                        .until(new ExpectedCondition<Boolean>() {
-                            @Override
-                            public Boolean apply(WebDriver driver) {
-                                return driver.findElements(By.cssSelector("#containment-portfolio li:not(.hidden)")).size() == 2;
-                            }
-                 });
-        assertThat(driver.findElements(By.cssSelector("#containment-portfolio li:not(.hidden)")).size()).isEqualTo(2);
+    	portofolioPage.clickOnWebLinkAndWaitUntilContainmentPortfolioContainsTwoElements();
+    	
+        assertThat(portofolioPage.getNumberOfVisibleElementsInContainmentPortfolio()).isEqualTo(2);
     }
 
 
--- src/test/java/org/duchess/selenium/part4/ex2/PortofolioPage.java
+++ src/test/java/org/duchess/selenium/part4/ex2/PortofolioPage.java
@@ -1,10 +1,47 @@
 package org.duchess.selenium.part4.ex2;
 
+import org.duchess.selenium.Util;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 /**
  * Utiliser des WebElement
  */
 public class PortofolioPage {
+    private final WebDriver driver;
+    
+    private WebElement web;
+    private WebElement containmentPortfolio;
 
+	public PortofolioPage(WebDriver driver) {
+		this.driver = driver;
+		driver.get(Util.getPortfolio2());
+		
+		web = driver.findElement(By.linkText("WEB"));
+		containmentPortfolio = driver.findElement(By.id("containment-portfolio"));
+	}
+	
+	public void clickOnWebLink() {
+		web.click();
+	}
+	
+	public void clickOnWebLinkAndWaitUntilContainmentPortfolioContainsTwoElements() {
+		clickOnWebLink();
+
+        (new WebDriverWait(driver, 10))
+                        .until(new ExpectedCondition<Boolean>() {
+                            @Override
+                            public Boolean apply(WebDriver driver) {
+                                return getNumberOfVisibleElementsInContainmentPortfolio() == 2;
+                            }
+                 });
+	}
+	
+	public int getNumberOfVisibleElementsInContainmentPortfolio() {
+		return containmentPortfolio.findElements(By.cssSelector("li:not(.hidden)")).size();
+	}
 
 }
