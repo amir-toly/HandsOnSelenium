22f2ccf745556f4bbb83dffb2e29f5bfae7e5717
Amir TOLY <amir.toly@gmail.com>
2014-05-12 08:10:55 +0200

    Complete part4 - exercise 1

--- src/test/java/org/duchess/selenium/part4/ex1/PortofolioPage.java
+++ src/test/java/org/duchess/selenium/part4/ex1/PortofolioPage.java
@@ -1,8 +1,33 @@
 package org.duchess.selenium.part4.ex1;
 
+import org.duchess.selenium.Util;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class PortofolioPage {
-    //private final WebDriver driver;
+    private final WebDriver driver;
 
+	public PortofolioPage(WebDriver driver) {
+		this.driver = driver;
+		driver.get(Util.getPortfolio2());
+	}
+	
+	public void clickOnWebLink() {
+		driver.findElement(By.ByLinkText.linkText("WEB")).click();
+	}
+	
+	public void clickOnWebLinkAndWaitUntilContainmentPortfolioContainsTwoElements() {
+		clickOnWebLink();
+
+        (new WebDriverWait(driver, 10))
+                        .until(new ExpectedCondition<Boolean>() {
+                            @Override
+                            public Boolean apply(WebDriver driver) {
+                                return driver.findElements(By.cssSelector("#containment-portfolio li:not(.hidden)")).size() == 2;
+                            }
+                 });
+	}
 
 }
